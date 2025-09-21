451cdcc37df7f84f26b99c9ac82cff123bebdb45
Anand Suthar <anandsuthar956@gmail.com>
2025-09-21 15:25:42 +0530

    fix: routing

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -7,23 +7,21 @@ import '../presentation/properties_screen/properties_screen.dart';
 import '../presentation/blog_screen/blog_screen.dart';
 
 class AppRoutes {
-  // TODO: Add your routes here
   static const String initial = '/';
   static const String splash = '/splash-screen';
+  static const String properties = '/properties-screen';
   static const String propertyDetails = '/property-details-screen';
   static const String emiCalculator = '/emi-calculator-screen';
   static const String webStories = '/web-stories-screen';
-  static const String properties = '/properties-screen';
   static const String blog = '/blog-screen';
 
   static Map<String, WidgetBuilder> routes = {
     initial: (context) => const SplashScreen(),
     splash: (context) => const SplashScreen(),
+    properties: (context) => const PropertiesScreen(),
     propertyDetails: (context) => const PropertyDetailsScreen(),
     emiCalculator: (context) => const EmiCalculatorScreen(),
     webStories: (context) => const WebStoriesScreen(),
-    properties: (context) => const PropertiesScreen(),
     blog: (context) => const BlogScreen(),
-    // TODO: Add your other routes here
   };
 }
