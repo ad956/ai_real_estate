aa25e58e91c097f6de2b2e938f02fe6210358609
Anand Suthar <anandsuthar956@gmail.com>
2025-09-21 18:56:02 +0530

    chore: add new routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import '../presentation/splash_screen/splash_screen.dart';
 import '../presentation/property_details_screen/property_details_screen.dart';
-import '../presentation/emi_calculator_screen/emi_calculator_screen.dart';
+import '../presentation/emi_calculator_screen/emi_calculator_screen_new.dart';
 import '../presentation/web_stories_screen/web_stories_screen.dart';
 import '../presentation/properties_screen/properties_screen.dart';
 import '../presentation/blog_screen/blog_screen.dart';
+import '../presentation/profile_screen/profile_screen.dart';
 
 class AppRoutes {
   static const String initial = '/';
@@ -14,6 +15,7 @@ class AppRoutes {
   static const String emiCalculator = '/emi-calculator-screen';
   static const String webStories = '/web-stories-screen';
   static const String blog = '/blog-screen';
+  static const String profile = '/profile-screen';
 
   static Map<String, WidgetBuilder> routes = {
     initial: (context) => const SplashScreen(),
@@ -23,5 +25,6 @@ class AppRoutes {
     emiCalculator: (context) => const EmiCalculatorScreen(),
     webStories: (context) => const WebStoriesScreen(),
     blog: (context) => const BlogScreen(),
+    profile: (context) => const ProfileScreen(),
   };
 }
