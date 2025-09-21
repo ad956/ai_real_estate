c4e399f514799206db5b742a1d66b81ceaeedfac
Anand Suthar <anandsuthar956@gmail.com>
2025-09-21 18:56:48 +0530

    feat: add navbar widget

---
+++ lib/widgets/shared_bottom_navbar.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
+
+import '../core/app_export.dart';
+
+class SharedBottomNavbar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const SharedBottomNavbar({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 10.h,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF1A1A2E),
+            Color(0xFF16213E),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+        borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.3),
+            blurRadius: 20,
+            offset: Offset(0, -10),
+          ),
+        ],
+      ),
+      child: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildNavItem(0, Icons.home, Icons.home_outlined, 'Home'),
+            _buildNavItem(1, Icons.auto_stories, Icons.auto_stories_outlined, 'Stories'),
+            _buildNavItem(2, Icons.article, Icons.article_outlined, 'Blog'),
+            _buildNavItem(3, Icons.calculate, Icons.calculate_outlined, 'EMI'),
+            _buildNavItem(4, Icons.person, Icons.person_outline, 'Profile'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(int index, IconData activeIcon, IconData inactiveIcon, String label) {
+    final isSelected = currentIndex == index;
+    
+    return GestureDetector(
+      onTap: () => onTap(index),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 2.5.w, vertical: 0.8.h),
+        decoration: BoxDecoration(
+          gradient: isSelected
+              ? LinearGradient(
+                  colors: [AppTheme.accentDark, AppTheme.primaryDark],
+                )
+              : null,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: isSelected
+              ? [
+                  BoxShadow(
+                    color: AppTheme.primaryDark.withValues(alpha: 0.4),
+                    blurRadius: 8,
+                    offset: Offset(0, 2),
+                  ),
+                ]
+              : null,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              isSelected ? activeIcon : inactiveIcon,
+              color: isSelected ? Colors.white : Colors.white.withValues(alpha: 0.6),
+              size: 22,
+            ),
+            SizedBox(height: 0.5.h),
+            Text(
+              label,
+              style: GoogleFonts.poppins(
+                color: isSelected ? Colors.white : Colors.white.withValues(alpha: 0.6),
+                fontSize: 10,
+                fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
